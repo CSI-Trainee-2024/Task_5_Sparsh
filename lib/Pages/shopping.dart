06ddeaa2cbc40998cc077b40b0700189228aec32
Sparsh2509 <sparshgupta250904@gmail.com>
2024-10-31 13:54:44 +0530

    Add cart template ui created

--- lib/Pages/shopping.dart
+++ lib/Pages/shopping.dart
@@ -15,21 +15,91 @@ class Shopping extends StatefulWidget {
 class _HomeState extends State<Shopping> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Cart",
-            style: TextStyle(fontSize: 20),
-          ),
-          // Container(
-            // color: Colors.green,
-          // )
-        ],
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Cart",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
       ),
+      Container(
+        width: screenWidth * 0.8,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: plateColor,
+        ),
+        child: Row(
+          children: [
+            Container(
+              child: Icon(Icons.check_box),
+            ),
+            SizedBox(
+              width: screenWidth * 0.02,
+            ),
+            Container(
+                height: screenWidth * 0.25,
+                width: screenWidth * 0.25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: plateColor.withOpacity(0.1),
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://static.toiimg.com/thumb/msid-112525506,width-1070,height-580,imgsize-1776962,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"),
+                        fit: BoxFit.cover))),
+            SizedBox(
+              width: screenWidth * 0.05,
+            ),
 
-    );
+            Column(
+              children: [
+                Text("Expresso"),
+                // SizedBox(
+                //   height: screenHeight*0.02,
+                // ),
+                Text("with milk"),
+                 SizedBox(
+                  height: screenHeight*0.02,
+                ),
+                Text("\$4.20")
+              ],
+
+            ),
+
+             SizedBox(
+              width: screenWidth * 0.05,
+            ),
+
+            Column(
+              children: [
+                IconButton(onPressed: (){}, icon: const Icon(Icons.add)),
+                SizedBox(
+                  height: 3,
+                ),
+                Text("2"),
+                SizedBox(
+                  height: 3,
+                ),
+                IconButton(onPressed: (){}, icon: const Icon(Icons.remove))
+
+
+
+              ],
+            )
+            
+            
+            
+
+
+          ],
+        ),
+      )
+    ]);
   }
 }
